7ea32d68a82d4e39d0504686e37c711464e1686e
razrabotal <goldsteals@gmail.com>
2016-05-30 05:30:52 +0300

    Add files via upload

---
+++ tests/test.datetime.php
@@ -0,0 +1,14 @@
+<meta charset="utf-8">
+<?php
+
+require('../timeago.inc.php');
+
+echo timeAgoInWords("2016/05/22 18:09:00");
+echo "<br>";
+echo timeAgoInWords("2016/05/20 23:05:00");
+echo "<br>";
+echo timeAgoInWords("2016/05/18 23:05:00");
+echo "<br>";
+echo timeAgoInWords("2016/03/18 23:05:00");
+
+?>
